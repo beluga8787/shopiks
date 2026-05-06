9706882b9a1463bf50b7265db59e2418f6a3d543
Gitler <GitlerZZZoMaxoPutin@company.com>
2026-05-06 18:28:53 +0300

    ahh le le le

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from 'express'
+import fs from 'fs'
+import multer from 'multer'
 
 const app = express()
 
+const upload = multer({dest: 'public/image'})
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req,res) => {
     res.render('index', {
@@ -14,9 +19,12 @@ app.get('/', (req,res) => {
 
 
 app.get('/products', (req, res) => {
+    const data = fs.readFileSync('data.json', 'utf-8')
+    const prods = JSON.parse(data)
     res.render('products', {
         tomy: 'Producti',
-        cssname: 'products'
+        cssname: 'product',
+        products: prods
     })
 })
 
@@ -26,4 +34,29 @@ app.get('/cart', (req, res) => {
         cssname: 'cart'
     })
 })
+app.get('/products/add', (req, res) => {
+    res.render('add-product', {
+        tomy: 'add',
+        cssname: 'add-product'
+    })
+})
+
+
+app.post('/products/add',upload.single('image'), (req,res) => {
+    const data = fs.readFileSync('data.json', 'utf-8')
+    const products = JSON.parse(data)
+
+    const newProd = {
+        id: products.length > 0 ? products.length + 1 : 1,
+        name: req.body.name,
+        price: Number(req.body.price),
+        image: '/image/' + req.file.filename
+    }
+    
+    products.push(newProd)
+    fs.writeFileSync('data.json', JSON.stringify(products,null,2))
+
+    res.redirect('/products')
+})
+
 app.listen(6752)
